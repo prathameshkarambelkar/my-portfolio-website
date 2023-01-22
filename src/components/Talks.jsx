a040bceeb000aa25f139b38bf5d8ee64e562bd59
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-01-23 00:48:58 +0530

    Changes title and icon of the website

--- src/components/Talks.jsx
+++ src/components/Talks.jsx
@@ -6,9 +6,7 @@ const Talks = () => {
       <section
         id="Talks"
         className="container flex items-center justify-center h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/TalksSmall.png')] lg:bg-[url('/Talks.png')] md:bg-[url('/Talks.png')] "
-      >
-        <h1 className="text-black text-3xl ">Talks</h1>
-      </section>
+      ></section>
     </>
   );
 };
