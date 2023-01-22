d8ad7f67869adcf3245c2e32d0cbcb05b449a8b5
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-01-22 14:52:07 +0530

    home bg image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     <>
       <section
         id="Home"
-        className="container  flex flex-col items-center  h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/HomeSmall.png')] lg:bg-[url('.../Home.png')] md:bg-[url('/Home.png')] "
+        className="container  flex flex-col items-center  h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/HomeSmall.png')] lg:bg-[url('/Home.png')] md:bg-[url('/Home.png')] "
       >
         <img
           className="rounded-full border-2 hover:border-green-600 border-black mt-28 items-center "
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ const Project = (props) => {
       <img
         width={300}
         height={300}
-        className="p-4  "
+        className="p-4"
         src="https://i.pinimg.com/474x/09/65/b9/0965b93f250920c1ee8a9a2b0ba0c291.jpg"
       />
       <h1 className="underline text-center">{props.name}</h1>
