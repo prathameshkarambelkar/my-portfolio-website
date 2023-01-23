5a2af806a9b15acbf0027ed2374a5720a20310b7
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-01-23 10:01:19 +0530

    Half Completed About Me

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -3,10 +3,27 @@ import React from "react";
 const Aboutme = () => {
   return (
     <>
-      <div
+      <section
         id="AboutMe"
-        className=" h-screen bg-fixed bg-center bg-cover bg-[url('/AboutMeSmall.png')]  md:bg-[url('/AboutMe1.png')]"
-      ></div>
+        className=" flex flex-col items-center justify-center  h-screen bg-fixed bg-center bg-cover bg-[url('/AboutMeSmall.png')]  md:bg-[url('/AboutMe1.png')]"
+      >
+        <div className="p-4  mt-6 mx-4 border-2 border-white rounded-md">
+          <h1 className="text-white text-md">
+            Hello, I'm Prathamesh, a third year grad pursuing Computer
+            Engineering , a software developer trying to solve real world
+            problems!
+          </h1>
+          <h1 className="text-md  text-white">
+            I usually work on Web Development and Android Development.{" "}
+          </h1>
+          <br />
+          <h1 className="text-md  text-white">
+            I am passionate about creating visually appealing and user-friendly
+            websites. Also I like to explore different technologies. 
+          </h1>
+          <h1 className="text-md text-white"></h1>
+        </div>
+      </section>
     </>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
     <>
       <section
         id="Home"
-        className="container  flex flex-col items-center  h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/HomeSmall.png')] lg:bg-[url('/home.png')] md:bg-[url('/Home.png')] "
+        className=" flex flex-col items-center  h-screen m-auto  bg-fixed bg-center bg-cover bg-[url('/HomeSmall.png')] lg:bg-[url('/home.png')] md:bg-[url('/Home.png')] "
       >
         <img
           className="rounded-full border-2 hover:border-green-600 border-black mt-28 items-center "
@@ -15,13 +15,13 @@ export const Home = () => {
           src="/developer_male.jpg"
         />
         <div className="writer inline-block">
-            <div className="writer-text mt-4  flex overflow-hidden border-r-4 text-black ">
-              <h1 className="text-center text-md">Hello 👋🏻, I'm Prathamesh,</h1>
-            </div>
+          <div className="writer-text mt-4  flex overflow-hidden border-r-4 text-black ">
+            <h1 className="text-center text-md">Hello 👋🏻, I'm Prathamesh</h1>
           </div>
-          <p className=" text-black text-2xl text-center font-poppins font-monofont-bold">
-            Android & Front-end Web Developer
-          </p>
+        </div>
+        <p className=" text-black text-2xl text-center font-poppins font-monofont-bold">
+          Software Developer
+        </p>
       </section>
     </>
   );
