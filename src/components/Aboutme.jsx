5af8fc0bb858091e6c5ccc1100e78a5a554ad8ed
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2023-01-27 23:28:52 +0530

    changed about me

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -9,19 +9,16 @@ const Aboutme = () => {
       >
         <div className="p-4  mt-6 mx-4 border-2 md:text-center border-white rounded-md">
           <h1 className="text-white text-md">
-            Hello, I'm Prathamesh, a third year grad pursuing Computer
+            Hello, I'm Prathamesh, a third year CS student pursuing Computer
             Engineering , a software developer trying to solve real world
-            problems!
-          </h1>
-          <h1 className="text-md  text-white">
-            I usually work on Web Development and Android Development.
+            problems usually working on Web Development and Android Development.
           </h1>
           <br />
           <h1 className="text-md  text-white">
             I am passionate about creating visually appealing and user-friendly
-            websites. Also I like to explore different technologies.
+            websites. Also like to explore different technologies.
           </h1>
-          <h1 className="text-md text-white">I believe in learning in public and share my learning.</h1>
+          <h1 className="text-md text-white">I believe in continous learning and share the same in public.</h1>
           
         </div>
         <a href="https://github.com/prathameshkarambelkar/resume/raw/main/PrathameshKarambelkarResume.pdf">
